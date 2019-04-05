cca03876fb9d6756d2cdfbde352365fda495d7fc
jaimefuentesp <jaimefuentesp@outlook.com>
2019-04-05 18:56:42 -0500

    final

---
+++ app/src/main/java/com/jaimefuentesp/tarearecyclerview/adapter/PageAdapter.java
@@ -0,0 +1,27 @@
+package com.jaimefuentesp.tarearecyclerview.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<Fragment> fragmentos;
+
+    public PageAdapter(FragmentManager fm,ArrayList<Fragment> fragmentos) {
+        super(fm);
+        this.fragmentos=fragmentos;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentos.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentos.size();
+    }
+}
